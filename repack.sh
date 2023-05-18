4cc546d9bff732e7ee5565d533694c029fbcca11
agpanarin <agpanarin@gmail.com>
2023-05-18 17:06:26 +0400

    vtdec: reset texture cache on renegotiate

---
+++ repack.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# clean
+rm -rf repack
+
+# extract
+pkgutil --expand-full ios-framework-1.22.2-arm64.pkg repack
+
+# pack
+cd repack/Payload/GStreamer.framework/Versions/Current
+tar -czf gstreamer-ios-headers-1.22.2.patch.1.tgz Headers
+tar -cJf gstreamer-ios-library-1.22.2.patch.1.tar.xz GStreamer
