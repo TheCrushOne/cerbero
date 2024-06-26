1f466285aedce2e8fe9dbb09d0761eddf7e3fbec
Alexander Krasheninnikov <contr239@gmail.com>
2024-06-26 13:32:16 +0300

    updated ver

--- repack.sh
+++ repack.sh
@@ -4,9 +4,9 @@
 rm -rf repack
 
 # extract
-pkgutil --expand-full ios-framework-1.22.2-universal.pkg repack
+pkgutil --expand-full ios-framework-1.24.2-universal.pkg repack
 
 # pack
 cd repack/Payload/GStreamer.framework/Versions/Current
-tar -czf gstreamer-ios-headers-1.22.2.patch.1.tgz Headers
-tar -cJf gstreamer-ios-library-1.22.2.patch.1.tar.xz GStreamer
+tar -czf gstreamer-ios-headers-1.24.2.patch.1.tgz Headers
+tar -cJf gstreamer-ios-library-1.24.2.patch.1.tar.xz GStreamer
